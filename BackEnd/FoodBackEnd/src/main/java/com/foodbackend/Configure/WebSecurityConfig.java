49485cca5f4005313ef8885e9ef9b923bbb5e05a
OlaGroup7 <olagroup7@gmail.com>
2022-07-26 14:48:37 +0530

    Packages renamed

--- BackEnd/FoodBackEnd/src/main/java/com/foodbackend/Configure/WebSecurityConfig.java
+++ BackEnd/FoodBackEnd/src/main/java/com/foodbackend/Configure/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.foodbackend.configure;
+package com.foodbackend.Configure;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
