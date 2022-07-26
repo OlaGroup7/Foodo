88dc500ca00b336e3c19ea4e41520a5e488e1b62
dhyey.zala <dhzala33@gmail.com>
2022-07-26 15:49:14 +0530

    "added home page methods in UserController,UserService and added a HomePageUserDetails model"

---
+++ BackEnd/FoodBackEnd/src/main/java/com/foodbackend/model/HomePageUserDetails.java
@@ -0,0 +1,22 @@
+package com.foodbackend.model;
+
+public class HomePageUserDetails {
+    private String Name;
+    private String Address;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
