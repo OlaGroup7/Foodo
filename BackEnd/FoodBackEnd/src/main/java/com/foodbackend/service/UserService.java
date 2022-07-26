8597a750cc6281f49ce1c02761218152a1ad53b9
Ashutosh Kumar <ak18saxena@gmail.com>
2022-07-26 16:18:29 +0530

    SignupResponse_added

--- BackEnd/FoodBackEnd/src/main/java/com/foodbackend/service/UserService.java
+++ BackEnd/FoodBackEnd/src/main/java/com/foodbackend/service/UserService.java
@@ -1,11 +1,30 @@
 package com.foodbackend.service;
 
+import com.foodbackend.model.HomePageUserDetails;
+import com.foodbackend.model.SignUpResponse;
+import com.foodbackend.model.User;
+import com.foodbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
 
+    String pepper = "qwerty123";
+
+    @Autowired
+    UserRepository userRepository;
+
+    public HomePageUserDetails fetchUserHomePageDetails(String userID)
+    {
+        HomePageUserDetails homePageUserDetails = new HomePageUserDetails(); //need to add the jpa methods like findbyID to fetch name and address
+        return homePageUserDetails;
+    }
+
     public SignUpResponse register(User user){
         SignUpResponse signUpResponse = new SignUpResponse();
-        User currentuser = userRepository.findByEmail(user.getEmail(),user.getPhone());
+        User currentuser = userRepository.findByEmail(user.getEmail());
         if(currentuser != null){
             signUpResponse.setFlag(false);
             signUpResponse.setMsg("User already exists");
@@ -16,8 +35,8 @@ public class UserService {
             user.setPassword(hashedPassword);
             user.setSalt(salt);
             userRepository.save(user);
-            signUpResponse.setStatus(true);
-            signUpResponse.setMessage("Signup Successful!!");
+            signUpResponse.setFlag(true);
+            signUpResponse.setMsg("Signup Successful!!");
             return signUpResponse;
         }
     }
